6cbec1b130d4feb198d0b690000da87142cead28
Bogdan <bogdan_uman@meta.ua>
2018-07-19 14:42:33 +0300

    fix tg chat_id unique && nanopul avarege hashrate

--- db/migrate/20180719171906_add_unique_index_chat_id_to_users.rb
+++ db/migrate/20180719171906_add_unique_index_chat_id_to_users.rb
@@ -3,19 +3,15 @@ class AddUniqueIndexChatIdToUsers < ActiveRecord::Migration[5.2]
     table_name = :users
 
     change_column_null table_name, :telegram_chat_id, :true
-    change_column_default table_name, :telegram_chat_id, nil
+    change_column_default table_name, :telegram_chat_id, from: 0, to: nil
 
-    # reversible do |direction|
-    #   direction.up do
-    #     execute <<-SQL.squish
-    #       UPDATE #{table_name} aa SET telegram_chat_id =
-    #         FROM monitoring_accounts bb WHERE aa.user_id = bb.user_id AND bb.user_id IS NOT NULL;
-    #       DELETE FROM #{table_name} WHERE monitoring_account_id IS NULL
-    #     SQL
-    #   end
-    # end
-
-
+    reversible do |direction|
+      direction.up do
+        execute <<-SQL.squish
+          UPDATE #{table_name} aa SET telegram_chat_id = NULL
+        SQL
+      end
+    end
 
     add_index table_name, :telegram_chat_id, unique: true
   end
